91c31ea7f2745eaa805d6b32266c656321108cfb
wsafight <984292420@qq.com>
2021-06-25 14:13:53 +0800

    feat: Pre build file path

--- rules/rule-default-import.js
+++ rules/rule-default-import.js
@@ -6,15 +6,23 @@ const actionsDir = path.join(__dirname, '../test/actions')
 const filePaths = []
 
 // The current import path has no file suffix and no file. Try to use the following suffixes in turn
-const fileSuffix = ['ts', 'js', 'json', 'node']
+const fileSuffix = ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'vue']
 
-// TODO Pre generate an array of strings for all files
-function setFilePaths (dirs) {
-    dirs.forEach(() => {
-
-    })
+// Pre generate an array of strings for all files
+function readFileList (dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach(item => {
+        var fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+        if (stat.isDirectory()) {
+            readFileList(path.join(dir, item));  //递归读取文件
+        } else {
+            filePaths.push(fullPath);
+        }
+    });
 }
 
+readFileList(actionsDir);
 
 module.exports = function (ast) {
     const platform = process.env.platform
@@ -25,14 +33,23 @@ module.exports = function (ast) {
             const match = node.match
             const filePath = match['filepath'][0].value
             if (filePath.startsWith('@actions')) {
-                const file = path.join(actionsDir, `/${platform}/${filePath.replace('@actions', '')}`) + '.js'
-                try {
-                    // Check whether the file exists. No exception is thrown
-                    fs.accessSync(file)
-                    // Replace imported path
+                const file = path.join(actionsDir, `/${platform}/${filePath.replace('@actions', '')}`)
+                // try {
+                //     // Check whether the file exists. No exception is thrown
+                //     fs.accessSync(file)
+                //     // Replace imported path
+                //     match['filepath'][0].node.value = filePath.replace('@actions', `@actions/${platform}`)
+                // } catch (err) {
+                //     console.error(`cannot find file ${file}`)
+                // }
+                const hasFile = filePaths.some(path => {
+                    if (path === file) {
+                        return true
+                    }
+                    return fileSuffix.some(suffix => path === `${file}.${suffix}`)
+                })
+                if (hasFile) {
                     match['filepath'][0].node.value = filePath.replace('@actions', `@actions/${platform}`)
-                } catch (err) {
-                    console.error(`cannot find file ${file}`)
                 }
             }
         })

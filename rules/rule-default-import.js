9b2b47a87af306de9aa20319b0d75c3655d07e5b
wsafight <984292420@qq.com>
2021-06-25 11:36:45 +0800

    refactor: Add ideas,Pre build file path

--- rules/rule-default-import.js
+++ rules/rule-default-import.js
@@ -3,6 +3,19 @@ const path = require("path")
 
 const actionsDir = path.join(__dirname, '../test/actions')
 
+const filePaths = []
+
+// The current import path has no file suffix and no file. Try to use the following suffixes in turn
+const fileSuffix = ['ts', 'js', 'json', 'node']
+
+// TODO Pre generate an array of strings for all files
+function setFilePaths (dirs) {
+    dirs.forEach(() => {
+
+    })
+}
+
+
 module.exports = function (ast) {
     const platform = process.env.platform
     return ast
@@ -14,8 +27,9 @@ module.exports = function (ast) {
             if (filePath.startsWith('@actions')) {
                 const file = path.join(actionsDir, `/${platform}/${filePath.replace('@actions', '')}`) + '.js'
                 try {
+                    // Check whether the file exists. No exception is thrown
                     fs.accessSync(file)
-                    console.log(file)
+                    // Replace imported path
                     match['filepath'][0].node.value = filePath.replace('@actions', `@actions/${platform}`)
                 } catch (err) {
                     console.error(`cannot find file ${file}`)
